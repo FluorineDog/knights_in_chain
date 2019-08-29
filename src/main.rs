2b7747a7b9f4f5826800cd394f89f5dcc8783d87
fluorinedog <fluorinedog@gmail.com>
2019-08-29 22:14:30 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+
+fn gen_fibo(n: usize) -> Vec<i64> {
+    let mut res = Vec::new();
+    assert!(n > 2);
+    let mut r0 = 0;
+    let mut r1 = 1;
+    for _i in 0..n {
+        res.push(r0);
+        let (r0_, r1_) = (r1, r0 + r1);
+        r0 = r0_;
+        r1 = r1_;
+    }
+    res
+}
+
 fn main() {
-    println!("Hello, world!");
+    let fibo_vec = gen_fibo(10);
+    println!("{:?}", fibo_vec);
+    // println!("Hello, world!");
 }
