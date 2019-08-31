77d8f84b4232d9d364423c3423ee9e1966cf2f03
fluorinedog <fluorinedog@gmail.com>
2019-09-01 03:20:26 +0800

    updaste

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn gen_fibo(n: usize) -> Vec<i64> {
     assert!(n > 2);
     let mut r0 = 0;
     let mut r1 = 1;
-    for _i in 0..n {
+    for _i in 0..(n + 1) {
         res.push(r0);
         let (r0_, r1_) = (r1, r0 + r1);
         r0 = r0_;
@@ -25,29 +25,77 @@ struct Meta {
     r0: i64, // pair sum = fib(n-1), left side
     r1: i64, // pair sum = fib(n), left side
     r2: i64, // pair sum = fib(n-1), left side
-    level: usize, 
-    value: i64, 
+    level: usize,
+    value: i64,
 }
 
-impl Meta{
-fn upgrade(&self) -> Meta {
-    let Meta{r0, r1, r2, level, value} = self.clone();
-    Meta{r0:r1, r1:r0 * 2 + r1, r2:r2 + r0 + 1, level:level + 1, value}
-}
+fn fibo(level: usize) -> i64 {
+    TABLE[level]
 }
 
 impl Meta {
-fn upgrade_leftmost(&self) -> Meta{
-    let Meta{r0, r1, r2, level, value} = self.clone();
-    assert!(value == TABLE[level]);
-    self.clone()
+    fn location(&self) -> usize {
+        (self.r0 + self.r1 + self.r2) as usize
+    }
 }
+
+impl Meta {
+    fn upgrade(&self) -> Meta {
+        let Meta {
+            r0,
+            r1,
+            r2,
+            level,
+            value,
+        } = self.clone();
+
+        Meta {
+            r0: r1,
+            r1: r0 * 2 + r1,
+            r2: r2 + r0 + 1,
+            level: level + 1,
+            value,
+        }
+    }
+}
+
+impl Meta {
+    fn upgrade_leftmost(&self) -> Meta {
+        assert!(self.value == fibo(self.level));
+        assert!(self.location() == 0);
+        let level = self.level + 1;
+        let value = fibo(level);
+        Meta {
+            r0: 0,
+            r1: 0,
+            r2: 0,
+            level,
+            value,
+        }
+    }
+}
+
+impl Meta {
+    fn upgrade_rightmost(&self) -> (Meta, bool) {
+        let Meta {
+            r0,
+            r1,
+            r2,
+            level,
+            value,
+        } = self.clone();
+        let flag = (fibo(level + 1) - fibo(level)) % 2 == 0;
+        if flag {
+            (self.clone(), flag)
+        } else {
+            (self.clone(), flag)
+        }
+    }
 }
 
 fn range_upscale(left: &Meta, right: &Meta) -> Option<(Meta, Meta)> {
     Some((left.clone(), right.clone()))
 }
-
 
 fn main() {
     let fibo_vec = gen_fibo(FIN);
